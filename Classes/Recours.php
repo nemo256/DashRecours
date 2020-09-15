4907c818255037b995046d0de6ad6266a1ab3beb
nemo256 <neggazimedlamine@gmail.com>
2020-09-15 12:37:23 +0100

    Deleted unecessary files

--- Classes/Recours.php
+++ Classes/Recours.php
@@ -64,7 +64,7 @@ class recours extends database
   // Checking for valid description (Prevent any input)! //
   private function checkDescription()
   {
-    if (preg_match('/^[a-z0-9 .\-]+$/i', $this->info['desc']))
+    if (preg_match('/^[a-zA-Z0-9 .\-]+$', $this->info['desc']))
       redirect (
         $GLOBALS['MSG']['IDD'],
         'warning', 
